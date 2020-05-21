a04beddd18b4c83f1055b7623d879c59ddb2d133
Steliyan Stoyanov <steliyan@st6.io>
2020-05-21 16:38:08 +0300

    Initial

---
+++ CacheManagerSpike/CacheManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheManagerSpike
+{
+    public class CacheManager<T>
+    {
+        private readonly MemoryCache cache;
+        private readonly Func<object, DateTime, Func<ICacheEntry, T>> createAbsoluteExpirationFactory;
+
+        public CacheManager(Func<object, T> factory)
+            : this(factory, new MemoryCacheOptions())
+        {
+        }
+
+        public CacheManager(Func<object, T> factory, MemoryCacheOptions options)
+        {
+            cache = new MemoryCache(options);
+
+            createAbsoluteExpirationFactory = (key, expireOn) => e =>
+            {
+                e.AbsoluteExpiration = expireOn;
+                return factory(key);
+            };
+        }
+
+        public T GetOrAdd(object key, DateTime expireOn)
+        {
+            var factory = createAbsoluteExpirationFactory(key, expireOn);
+            return cache.GetOrCreate(key, factory);
+        }
+    }
+}
---
+++ CacheManagerSpike/CacheManagerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CacheManagerSpike
+{
+    public class CacheManagerTests
+    {
+        private readonly CacheManager<string> cacheManager;
+        private readonly Func<object, string> factory;
+        private int called;
+
+        public CacheManagerTests()
+        {
+            called = 0;
+            factory = key =>
+            {
+                called++;
+                return $"{key} {called}";
+            };
+            cacheManager = new CacheManager<string>(factory);
+        }
+
+        [Fact]
+        public async Task ShouldUseCache_WhenSingleValueIsNotExpired()
+        {
+            var cacheManager = new CacheManager<string>(factory);
+
+            var k11 = cacheManager.GetOrAdd("key1", DateTime.Now.AddSeconds(1));
+            await Task.Delay(100);
+            var k12 = cacheManager.GetOrAdd("key1", DateTime.MaxValue);
+
+            Assert.Equal("key1 1", k11);
+            Assert.Equal("key1 1", k12);
+            Assert.Equal(1, called);
+        }
+
+        [Fact]
+        public async Task ShouldNotUseCache_WhenSingleValueIsExpired()
+        {
+            var cacheManager = new CacheManager<string>(factory);
+
+            var k11 = cacheManager.GetOrAdd("key1", DateTime.Now.AddSeconds(1));
+            await Task.Delay(1000);
+            var k12 = cacheManager.GetOrAdd("key1", DateTime.MaxValue);
+
+            Assert.Equal("key1 1", k11);
+            Assert.Equal("key1 2", k12);
+            Assert.Equal(2, called);
+        }
+
+        [Fact]
+        public void ShouldUseCache_WithTwoValues()
+        {
+            var cacheManager = new CacheManager<string>(factory);
+
+            var k11 = cacheManager.GetOrAdd("key1", DateTime.Now.AddSeconds(1));
+            var k21 = cacheManager.GetOrAdd("key2", DateTime.Now.AddSeconds(1));
+
+            Assert.Equal("key1 1", k11);
+            Assert.Equal("key2 2", k21);
+            Assert.Equal(2, called);
+        }
+
+        [Fact]
+        public async Task ShouldNotUseCache_WithTwoValuesAndOneIsExpired()
+        {
+            var cacheManager = new CacheManager<string>(factory);
+
+            var k11 = cacheManager.GetOrAdd("key1", DateTime.Now.AddSeconds(1));
+            var k21 = cacheManager.GetOrAdd("key2", DateTime.Now.AddSeconds(1));
+            await Task.Delay(1000);
+            var k12 = cacheManager.GetOrAdd("key1", DateTime.Now.AddSeconds(1));
+
+            Assert.Equal("key1 1", k11);
+            Assert.Equal("key2 2", k21);
+            Assert.Equal("key1 3", k12);
+            Assert.Equal(3, called);
+        }
+    }
+}
